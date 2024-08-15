5971b109a0496e9810ad9ca59676b1bf787c7965
hurluis <luismiguelgiraldohurtado@gmail.com>
2024-08-14 21:44:58 -0500

    enter

--- ADA #1/Recursivo.py
+++ ADA #1/Recursivo.py
@@ -44,6 +44,7 @@ class ListaEnlazada:
             if letra_nueva <= nodo.letra:
                 nuevo_nodo = Nodo(nodo.numero, nodo.letra)
                 nuevo_nodo.siguiente = nodo.siguiente
+                nodo.numero=nodo.numero-1
                 nodo.letra = letra_nueva
                 nodo.siguiente = nuevo_nodo
             else:
@@ -98,12 +99,12 @@ lista = ListaEnlazada()
 
 lista.insertar(0, 'A')
 lista.insertar(1, 'B')
-lista.insertar(3, 'W')
-lista.insertar(5, 'X')
-lista.insertar(7, 'U')
-lista.insertar(8, 'V')
-lista.insertar(9, 'R')
-lista.insertar(12, 'D')
+lista.insertar(4, 'W')
+lista.insertar(7, 'X')
+lista.insertar(10, 'U')
+lista.insertar(13, 'V')
+lista.insertar(16, 'R')
+lista.insertar(19, 'D')
 
 print("Lista inicial:")
 lista.imprimir()
